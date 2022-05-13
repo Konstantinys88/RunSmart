3833ddc1021bc0de36bf4d8bf0259ea4cc59ae01
Konstantin0 <maschkovc@yandex.ru>
2022-05-13 18:33:07 +0300

    php отправка сообщений на почту

--- src/js/script.js
+++ src/js/script.js
@@ -111,6 +111,29 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+7 (999) 999-9999");
 
+    $('form').submit(function(e){
+        e.preventDefault();
+
+        if(!$(this).valid()){
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            //потестировать эту часть 
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('redet');
+        });
+        return false;
+
+    });
+
 });
 
 
